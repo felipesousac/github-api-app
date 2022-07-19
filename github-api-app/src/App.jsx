fc72ace2a068ca6e1c43dd842a9fd63cf6914be5
felipesousac <claudioflsousa@gmail.com>
2022-07-18 22:03:57 -0300

    Create project

---
+++ github-api-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <main>
+      <Layout>
+        <div>User</div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
+  )
+}
+
+export default App
